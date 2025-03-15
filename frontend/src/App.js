80d21beff9a5bcf7fd09d4ff7264539c70ec7cec
Rina Rushiti <149533412+RinaRushiti@users.noreply.github.com>
2025-03-15 19:13:11 +0100

    created React project
    installed packages and structured folders

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "../src/api/axiosConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <p> hello !!</p>
+    </div>
+  );
+}
+
+export default App;
