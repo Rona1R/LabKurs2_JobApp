57ff437e03633a1d3a8d8892baa06486dc38df08
Rita Shkodra <149578440+RitaShkodra@users.noreply.github.com>
2025-03-18 16:07:03 +0100

    Added Register page using mdb react template
    + included register page in routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import UserLayout from "pages/User/UserLayout";
 import Index from "pages/Home";
 import Dashboard from "pages/Dashboard/Dashboard";
 import Categories from "pages/Dashboard/Administrator/Categories/Categories";
+import Register from "pages/Authentication/Register/Register";
 
 const routes = [
   {
@@ -25,6 +26,7 @@ const routes = [
       },
     ],
   },
+  { path: "/register", element: Register }
 ];
 
 export default routes;
