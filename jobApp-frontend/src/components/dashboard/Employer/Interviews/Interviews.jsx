3b782934c66ed4fb316b8b664575deb1a5c51d94
Rona Rushiti <71156882+Rona1R@users.noreply.github.com>
2025-05-16 00:21:25 +0200

    customized calendar

--- jobApp-frontend/src/components/dashboard/Employer/Interviews/Interviews.jsx
+++ jobApp-frontend/src/components/dashboard/Employer/Interviews/Interviews.jsx
@@ -1,18 +1,37 @@
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import { Box } from '@mui/material';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { Box } from "@mui/material";
+import "./style/Calendar.css";
 
 export function Interviews() {
+  const handleDateClick = (arg) => {
+    alert(arg.dateStr);
+  };
+
   return (
-    <Box sx={{mx:4}}>
+    <Box sx={{ mx: 4 }}>
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
+        allDaySlot={false}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        eventClassNames={(arg) => {
+          return ['interview-event'];
+        }}
         events={[
-          { title: 'Meeting', start: '2024-05-15T10:30:00', end: '2024-05-15T12:00:00' },
-          { title: 'Lunch', start: '2024-05-16T12:00:00', color: 'green' }
+          {
+            title: "Interview 1",
+            start: "2025-05-15T10:00:00",
+            end: "2025-05-15T10:30:00",
+          },
+          {
+            title: "Interview 2",
+            start: "2025-05-15T13:30:00",
+            end: "2025-05-15T14:30:00",
+          },
         ]}
+        dateClick={handleDateClick}
       />
     </Box>
   );
